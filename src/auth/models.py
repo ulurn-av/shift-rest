a4e83423fc1954f1c8416efacf3805aa68dfa00b
ulurn-av <ulurn@yandex.ru>
2024-05-22 01:26:33 +0700

    models rebuilding

--- src/auth/models.py
+++ src/auth/models.py
@@ -9,17 +9,10 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str | None] = mapped_column(unique=True, index=True)
     name: Mapped[str]
     middle_name: Mapped[str]
     last_name: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str]
-    salary_id: Mapped[int] = mapped_column(ForeignKey("salaries.id", ondelete="SET NULL"))
-
-
-class Salary(Base):
-    __tablename__ = "salaries"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    amount: Mapped[int]
-    date: Mapped[date]
+    salary_id: Mapped[int | None] = mapped_column(ForeignKey("salaries.id", ondelete="SET NULL"))
